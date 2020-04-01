605e92086db509e4a4ee53f1dd73bc141f3c4fe7
Ivan <naumau.ivan@gmail.com>
2020-04-02 00:34:19 +0300

    point4

--- delete.php
+++ delete.php
@@ -1,15 +1,21 @@
 <?php
 $id = $_GET['id'];
 
-$db_user = "default";
-$db_pass ="secret";
-$dsn = 'pgsql:host=192.168.100.169; dbname=default';
-$pdo = new PDO($dsn, $db_user, $db_pass);
+/**
+ * @param $id
+ */
+function deleteTask($id) {
+    $db_user = "default";
+    $db_pass ="secret";
+    $dsn = 'pgsql:host=192.168.100.198; dbname=default';
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    $sql = 'DELETE FROM tasks WHERE id=:id';
+    $statement = $pdo->prepare($sql);
+    $statement->bindParam(":id", $id);
+    $statement->execute();
+    header('Location: /');
+};
 
-$sql = 'DELETE FROM tasks WHERE id=:id';
-$statement = $pdo->prepare($sql);
-$statement->bindParam(":id", $id);
-$statement->execute();
+deleteTask($id);
 
 
-header('Location: /');
--- update.php
+++ update.php
@@ -5,13 +5,16 @@ $data = [
     "title" =>  $_POST['title'],
     "content"   =>  $_POST['content']
 ];
+function updateTask ($data) {
+    $db_user = "default";
+    $db_pass ="secret";
+    $dsn = 'pgsql:host=192.168.100.198; dbname=default';
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    $sql = 'UPDATE tasks SET title=:title, content=:content WHERE id=:id';
+    $statement = $pdo->prepare($sql);
+    $statement->execute($data); // true || false
 
-$db_user = "default";
-$db_pass ="secret";
-$dsn = 'pgsql:host=192.168.100.169; dbname=default';
-$pdo = new PDO($dsn, $db_user, $db_pass);
-$sql = 'UPDATE tasks SET title=:title, content=:content WHERE id=:id';
-$statement = $pdo->prepare($sql);
-$statement->execute($data); // true || false
+    header("Location: /"); exit;
+}
 
-header("Location: /"); exit;
+updateTask ($data);
